22f5796e724819d3f3f1ef7aff876aef8bdfa3a9
Drazen Buljovcic <drazen.buljovcic@getyourguide.com>
2025-04-17 20:41:02 +0200

    log

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { Octokit } from "octokit";
 const main = () => {
   try {
     const octokit = new Octokit();
+    console.log(import.meta);
+    console.log(import.meta.url);
+    console.log(import.meta.resolve);
   } catch (error) {
     console.error(error);
   }
