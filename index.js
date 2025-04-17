db92be6ff1cfc23bfb8c7f809b22f2aa2b7fc05e
Drazen Buljovcic <drazen.buljovcic@getyourguide.com>
2025-04-17 20:42:01 +0200

    log oct

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const main = () => {
     console.log(import.meta);
     console.log(import.meta.url);
     console.log(import.meta.resolve);
+    console.log(import.meta.resolve("octokit"));
+    console.log(import.meta.resolve("octokit/index.js"));
   } catch (error) {
     console.error(error);
   }
