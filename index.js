fcccb7820ab3fa0c53bc2c0d573a6f06dd7762b1
Drazen Buljovcic <drazen.buljovcic@getyourguide.com>
2025-04-17 20:57:49 +0200

    log oct

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const main = () => {
     console.log(import.meta);
     console.log(import.meta.url);
     console.log(import.meta.resolve);
-    try {
-      console.log(import.meta.resolve("octokit"));
-    } catch (error) {
-      console.error("Error resolving 'octokit':", error);
-      console.log(import.meta.resolve("octokit/index.js"));
-    }
+
+    console.error("Error resolving 'octokit':", error);
+
+    console.log(import.meta.resolve("octokit"));
+    console.log(import.meta.resolve("octokit/index.js"));
   } catch (error) {
     console.error(error);
   }
